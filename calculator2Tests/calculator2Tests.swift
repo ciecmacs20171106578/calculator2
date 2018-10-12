89a78f7355804ca12f3cb367af813a074b3f0d2f
ciecmacs20171106578 <m15886077136@163.com>
2018-10-12 20:40:05 +0800

    Initial Commit

---
+++ calculator2Tests/calculator2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  calculator2Tests.swift
+//  calculator2Tests
+//
+//  Created by s20171106578 on 2018/10/12.
+//  Copyright © 2018年 s20171106578. All rights reserved.
+//
+
+import XCTest
+@testable import calculator2
+
+class calculator2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
